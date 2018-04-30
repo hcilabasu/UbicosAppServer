54498fe3da21cac2bb3933acc745d1d1f58f4d02
Ishrat Ahmed <ishratahmed@Ishrats-MacBook-Pro.local>
2018-04-29 21:58:35 -0700

    gallery upload image to server

--- textbook/app/static/js/gallery.js
+++ textbook/app/static/js/gallery.js
@@ -49,14 +49,16 @@
       // }
     }
 
-    function changeImage(current) {
-      var imagesNumber = 5;
+//    function changeImage(current) {
+//      var imagesNumber = 5;
+//
+//      for (i=1; i<=imagesNumber; i++) {
+//        if (i == current) {
+//          document.getElementById("normal" + current).style.display = "block";
+//        } else {
+//          document.getElementById("normal" + i).style.display = "none";
+//        }
+//      }
+//    }
 
-      for (i=1; i<=imagesNumber; i++) {
-        if (i == current) {
-          document.getElementById("normal" + current).style.display = "block";
-        } else {
-          document.getElementById("normal" + i).style.display = "none";
-        }
-      }
-    }
+//
