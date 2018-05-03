34d0627c82a071d58d9a8a758b1aca69458dbddb
Victor Girotto <victor.girotto@outlook.com>
2018-05-03 16:46:46 -0700

    Implement video view

--- textbook/app/static/js/digTextBook.js
+++ textbook/app/static/js/digTextBook.js
@@ -1,3 +1,16 @@
 $(function(){
     
+    console.log('page load');
+    
+    $('.page a').on('touch click', function(){
+        var type = $(this).attr('class').replace('activity-button','').trim();
+        $('.card.' + type).addClass('active');
+        // $('.card.' + type).css('transform','none');
+        console.log('touch');
+    });
+
+    $('.close-card').on('touch click', function(){
+        $(this).closest('.card').removeClass('active');
+    });
+
 })
