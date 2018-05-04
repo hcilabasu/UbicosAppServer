9a53368e86558ca3a12231253af1b8d63e5b04d8
iamaureen <ishrat.ahmed@asu.edu>
2018-05-03 17:43:10 -0700

    user logout

--- textbook/app/urls.py
+++ textbook/app/urls.py
@@ -1,11 +1,12 @@
 
-
+from django.contrib.auth import views as auth_views
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.login_form, name='login_form'),
     url('login', views.login, name='login'),
+    url('logout', auth_views.logout, {'next_page': '/'}, name='logout'),
     url('index', views.index, name='index'),
     url('uploadImage', views.uploadImage, name='uploadImage'),
     url('getImage', views.getImage, name='getImg'),
