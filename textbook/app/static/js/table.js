84e2902c28b96fbf52acb2c97989b5bb0a9385cc
hshibani <mercury26th@gmail.com>
2018-06-12 20:27:51 -0700

    added a table for taking user input for graph before plotting

--- textbook/app/static/js/table.js
+++ textbook/app/static/js/table.js
@@ -1,18 +1,30 @@
 $(function(){
-
-var ctx = $('#time-distance-chart');
-var graph = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["1", "2", "3", "4", "5", "6"],
-        datasets: [{
-            label: "Time-Distance",
-            data: [5, 10, 15, 20, 25, 30],
-            fill: false,
-            borderColor: '#07C',
-        }]
-    }
-});
-
-
+    $('#plot_btn').click(function(e){
+        var times = [];
+        var distances = [];
+        var n = 0;
+        $("table td").each(function(){
+            if (n > 4) {
+                textval = $(this).find(":text").val();
+                distances.push(textval);
+            } else {
+                textval = $(this).find(":text").val();
+                times.push(textval);
+            }
+            n++;
+        })
+        var graph = new Chart($('#time-distance-chart'), {
+            type: 'line',
+            data: {
+                labels: times,
+                datasets: [{
+                    label: "Time-Distance",
+                    data: distances,
+                    fill: false,
+                    borderColor: '#07C',
+                 }]
+            }
+        });
+        graph.draw();
+    })
 })
