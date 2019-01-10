cd20ec1cf7117042af36be295fe2ceaf8cc358be
iamaureen <ishrat.ahmed@asu.edu>
2019-01-09 23:30:42 -0500

    ka-answer-copy-to-clipboard-demo

--- textbook/app/static/js/kaform.js
+++ textbook/app/static/js/kaform.js
@@ -4,13 +4,28 @@
 
 $(function(){
 
+    $('#ka-showAnsweredQues').hide();
 
-
-   $('#ka-submit').click(function(){
-        $('#ka-form-containter').hide();
-   })
-
-
+    ka_submit_button();
+    copy_ka_text_button();
 
 })
 
+
+var ka_submit_button = function(){
+    $('#ka-submit').click(function(){
+
+        $('#ka-form-containter').hide();
+        $('#ka-showAnsweredQues').show();
+   })
+}
+
+var copy_ka_text_button = function(){
+    $('.ka-row-copy-button').click(function(){
+        //https://jqueryhouse.com/copy-data-to-clipboard-using-jquery/
+        var copied_text = $(this).parent().siblings().select();
+        console.log(copied_text);
+        document.execCommand("copy");
+        alert("Copied the text: " + copied_text.text());
+    })
+}
