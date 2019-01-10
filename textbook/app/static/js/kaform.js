d20db6cba753fcfe21cb34770ce0febfaa196ea6
iamaureen <ishrat.ahmed@asu.edu>
2019-01-10 01:53:02 -0500

    copy to clipboard - paste shows copied value

--- textbook/app/static/js/kaform.js
+++ textbook/app/static/js/kaform.js
@@ -23,9 +23,18 @@ var ka_submit_button = function(){
 var copy_ka_text_button = function(){
     $('.ka-row-copy-button').click(function(){
         //https://jqueryhouse.com/copy-data-to-clipboard-using-jquery/
-        var copied_text = $(this).parent().siblings().select();
+        var copied_text = $(this).parent().siblings().text();
         console.log(copied_text);
+
+        var value = '<input value="'+ copied_text +'" id="selVal" />';
+        $(value).insertAfter($(this));
+        $("#selVal").select(); //select works for input //https://stackoverflow.com/questions/50941892/copy-to-clipboard-value-of-selected-option
         document.execCommand("copy");
-        alert("Copied the text: " + copied_text.text());
+        $('div#ka-showAnsweredQues').find("#selVal").remove();
+
+        alert("Copied the text: " + copied_text);
+
+
+
     })
 }
