57dbaf756f9a806f1184d69d43d9f1b4850bdd6d
iamaureen <ishrat.ahmed@asu.edu>
2019-02-28 20:26:44 -0500

    ka-talk moves update, ka-ssubmit button added-handled empty input

--- textbook/app/static/js/kaform.js
+++ textbook/app/static/js/kaform.js
@@ -6,6 +6,8 @@ $(function(){
     //hide black image src in the beginning
     $('img#ka-image').hide();
 
+    ka_submit_button();
+
 
     //ka_submit_button();
     copy_ka_text_button();
@@ -41,31 +43,31 @@ $(function(){
               });
      });
 
-    //handle textarea on focus out
-    $('#KAAnswer').blur(function() {
-
-            //TODO: add user log event; user log event will capture multiple attempts but model will store the latest answer
-
-            var isRadioBtnChecked = $("input[name='ka-response-type']").prop('checked');
-            //will return false if none of the radio button is not checked
-            //will return true if one of the radio button is checked
-
-            var user_response = $('#KAAnswer').val();
-            //console.log('user response::',user_response)
-
-            var ka_radio_input_type = $("input[name='ka-response-type']:checked").val();
-            //console.log('ka-response-type', ka_radio_input_type);
-
-            showPrompt(user_response);
-
-            saveKAresponseToDB(activity_id, ka_imgID, ka_radio_input_type, user_response);
-
-            $('.ka-answer-p').text(user_response)
-
-            //show the copy button since answer is posted
-            $('#ka-showAnsweredQues').show();
-
-        });
+//    //handle textarea on focus out
+//    $('#KAAnswer').blur(function() {
+//
+//            //TODO: add user log event; user log event will capture multiple attempts but model will store the latest answer
+//
+//            var isRadioBtnChecked = $("input[name='ka-response-type']").prop('checked');
+//            //will return false if none of the radio button is not checked
+//            //will return true if one of the radio button is checked
+//
+//            var user_response = $('#KAAnswer').val();
+//            //console.log('user response::',user_response)
+//
+//            var ka_radio_input_type = $("input[name='ka-response-type']:checked").val();
+//            //console.log('ka-response-type', ka_radio_input_type);
+//
+//            showPrompt(user_response);
+//
+//            saveKAresponseToDB(activity_id, ka_imgID, ka_radio_input_type, user_response);
+//
+//            $('.ka-answer-p').text(user_response)
+//
+//            //show the copy button since answer is posted
+//            $('#ka-showAnsweredQues').show();
+//
+//        });
 
 
     $('.moreinfobtn').click(function(e){
@@ -102,23 +104,41 @@ var readURL_ka = function(input) {
     }
 
 //handle user answer submit
-//var ka_submit_button = function(){
-//    $('#ka-submit').click(function(e){
-//        e.preventDefault();
-//
-//        console.log("user entered: ", $('#KAAnswer').val())
-//        //TODO: save the answer in database
-//        saveKAresponseToDB(1, $('#KAAnswer').val());
-//
-//        //hide one div and show the other
-//        $('#ka-form-containter').hide();
-//        $('#ka-showAnsweredQues').show();
-//
-//        //TODO: get the student response and set the answer to the p tag
-//
-//        $('.ka-answer-p').text($('#KAAnswer').val())
-//   })
-//}
+var ka_submit_button = function(){
+    $('#ka-submit').click(function(e){
+        e.preventDefault();
+
+        //TODO: add user log event; user log event will capture multiple attempts but model will store the latest answer
+
+
+        var user_response = $('#KAAnswer').val();
+        //console.log('user response::',user_response)
+
+        var ka_radio_input_type = $("input[name='ka-response-type']:checked").val();
+
+        //console.log('ka-response-type', ka_radio_input_type);
+
+        if(user_response.length!=0){
+            showPrompt(user_response);
+        }
+
+
+        if($("input[name='ka-response-type']").is(':checked') && user_response.length!=0) {
+
+            saveKAresponseToDB(activity_id, ka_imgID, ka_radio_input_type, user_response);
+        }
+        else{alert("please enter all the values")}
+
+        
+
+
+
+        $('.ka-answer-p').text(user_response)
+
+        //show the copy button since answer is posted
+        $('#ka-showAnsweredQues').show();
+   })
+}
 
 //method to copy student answer using a button
 var copy_ka_text_button = function(){
